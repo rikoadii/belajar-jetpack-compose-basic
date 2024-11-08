ddea907ab13593a7a933b5370fbf308167feeb55
rikoadii <radi60372@gmail.com>
2024-11-08 14:14:49 +0800

    Learning Basics Jetpack Compose

---
+++ app/src/main/java/com/belajarjetpackcompose/basicscodelab/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.belajarjetpackcompose.basicscodelab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
